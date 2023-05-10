aeea98f21f3d596c85a15394d7661841876ea574
Vova Pol <ya-chai777@yandex.ru>
2023-05-10 19:26:18 +0300

    добавил компонент Navigation, поправил конфиг Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Favourites from './pages/Favourites';
+import Navigation from './components/Navigation';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/favourites" element={<Favourites />}></Route>
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/favourites" element={<Favourites />}></Route>
+      </Routes>
+    </>
   );
 }
