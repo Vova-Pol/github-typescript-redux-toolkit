462d941a8ab1ca7d37e7813b63fca0d91bf5f61d
Vova Pol <ya-chai777@yandex.ru>
2023-05-11 17:48:51 +0300

    написал логику главной страницы

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchUsersQuery } from '../store/github/github.api';
+import { useState } from 'react';
+import { useDebouce } from '../hooks/debounce';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [search, setSearch] = useState('');
+  const debounced = useDebouce(search);
+  const [dropdown, setDropdown] = useState(false);
+  const { isError, isLoading, isSuccess, data } = useSearchUsersQuery(
+    debounced,
+    {
+      skip: debounced.length < 3,
+    },
+  );
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 3);
+  }, [debounced]);
+
+  return (
+    <div className="flex flex-col items-center pt-[20px] mx-auto h-screen w-screen relative">
+      {isError && (
+        <p className="text-center text-red-500">Server responded with Error</p>
+      )}
+
+      <div className="w-[560px]">
+        <input
+          className="border py-2 px-4 w-full h-[42px]"
+          type="text"
+          value={search}
+          onChange={(evt) => setSearch(evt.target.value)}
+        ></input>
+      </div>
+
+      {dropdown && (
+        <ul className="max-h-[200px] w-[560px] shadow-md bg-white overflow-y-scroll">
+          {isLoading && <p className="text-center">Loading...</p>}
+          {data?.map((user) => (
+            <li
+              key={user.id}
+              className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+            >
+              {user.login}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
