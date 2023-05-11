488ecd3d905e3832bb9286842a409f88616b885b
Vova Pol <ya-chai777@yandex.ru>
2023-05-11 17:48:33 +0300

    кастомный хук задержки

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export function useDebouce(value: string, delay: number = 500): string {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounced(value);
+    }, delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+}
