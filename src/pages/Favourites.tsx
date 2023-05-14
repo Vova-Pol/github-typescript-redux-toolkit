1f43c2d16cf505ad0250750cdd3857d607dbac60
Vova Pol <ya-chai777@yandex.ru>
2023-05-14 12:21:54 +0300

    написал компонент Favourites

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useAppSelector } from '../hooks/redux';
 
 export default function Favourites() {
-  return <div>Favourites</div>;
+  const { favourites } = useAppSelector((state) => state.github);
+
+  if (!favourites.length) {
+    return <p>No items yet</p>;
+  }
+
+  return (
+    <div className="flex flex-col items-center pt-[20px] mx-auto h-screen w-screen relative">
+      <ul className="list-none">
+        {favourites.map((f) => (
+          <li key={f}>
+            <a href={f} target="_blank">
+              {f}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
